8619cf1753eb05bae42745943754f9c5f6cccd19
maso0310 <balimon@hotmail.com>
2019-01-18 15:04:23 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+# Channel Secret
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
