73d9fb1cdc21cfd0d68b188d9555723d1bee91a5
maso0310 <balimon@hotmail.com>
2019-02-24 11:00:55 +0800

    'ok'

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = VideoSendMessage(
-        original_content_url = "https://i.imgur.com/8j32EDa.mp4",
+        original_content_url = "https://www.youtube.com/watch?v=f5QlVGfjWKI",
         preview_image_url = "https://img.shop.com/Image/260000/263400/263424/products/1477123666__175x175__.jpg"
     )
     #event.message.text就是用戶傳來的文字訊息
