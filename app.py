5e229525cd51c085605a0446a0caff39e22bc7e8
maso0310 <balimon@hotmail.com>
2019-02-24 10:25:18 +0800

    'ok'

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = VideoSendMessage(
-        original_content_url = "https://m.youtu.be/i8ncIcJs-LA",
-        preview_image_url = "https://i.ytimg.com/vi/i8ncIcJs-LA/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAUrKQjcvz9TSAMfsMbyV-kcOetWQ"
+        original_content_url = "https://youtu.be/i8ncIcJs-LA.mp4",
+        preview_image_url = "https://i.ytimg.com/vi/i8ncIcJs-LA/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAUrKQjcvz9TSAMfsMbyV-kcOetWQ.jpg"
     )
     #event.message.text就是用戶傳來的文字訊息
     line_bot_api.reply_message(event.reply_token, message)
