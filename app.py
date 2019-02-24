5e492f05d6cb806544e8c629129dc106380f38fa
maso0310 <balimon@hotmail.com>
2019-02-24 09:34:24 +0800

    'ok'

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text="https://www.google.com/search?q="+event.message.text)
+    message = TextSendMessage(text= event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
