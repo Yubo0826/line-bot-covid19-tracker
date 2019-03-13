587f387145d9310767721739472857d391545961
maso0310 <balimon@hotmail.com>
2019-03-13 22:11:04 +0800

    'ok'

--- message.py
+++ message.py
@@ -177,28 +177,28 @@ def image_carousel_message1():
         template=ImageCarouselTemplate(
             columns=[
                 ImageCarouselColumn(
-                    image_url="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg",
+                    image_url="https://i.imgur.com/uKYgfVs.jpg",
                     action=URITemplateAction(
                         label="新鮮水果",
                         uri="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg"
                     )
                 ),
                 ImageCarouselColumn(
-                    image_url="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg",
+                    image_url="https://i.imgur.com/QOcAvjt.jpg",
                     action=URITemplateAction(
                         label="新鮮蔬菜",
                         uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
                     )
                 ),
                 ImageCarouselColumn(
-                    image_url="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG",
+                    image_url="https://i.imgur.com/Np7eFyj.jpg",
                     action=URITemplateAction(
                         label="可愛狗狗",
                         uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
                     )
                 ),
                 ImageCarouselColumn(
-                    image_url="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg",
+                    image_url="https://i.imgur.com/QRIa5Dz.jpg",
                     action=URITemplateAction(
                         label="可愛貓咪",
                         uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
