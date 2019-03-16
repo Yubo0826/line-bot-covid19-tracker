5437b6c207db4ac8952d9bec73182b36e6ba4923
maso0310 <balimon@hotmail.com>
2019-03-16 23:08:44 +0800

    'ok'

--- app.py
+++ app.py
@@ -55,6 +55,9 @@ def handle_message(event):
     elif '圖片畫廊' in msg:
         message = test()
         line_bot_api.reply_message(event.reply_token, message)
+    elif '功能列表' in msg:
+        message = function_list()
+        line_bot_api.reply_message(event.reply_token, message)
     else:
         pass
 
