4e6bcc1052b60dde742ec16c595b1d13f82d7ab7
maso0310 <36530475+maso0310@users.noreply.github.com>
2019-05-03 13:29:22 +0800

    Update app.py
    ok

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     else:
         message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message
+        line_bot_api.reply_message(event.reply_token, message)
 
 
 import os
